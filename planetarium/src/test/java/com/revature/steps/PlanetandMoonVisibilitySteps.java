cbf1e84df767ff01d6710fcd98508c0a96fc6ac8
Thomas Kay <thomas605@revature.net>
2024-11-08 11:43:15 -0500

    Fixing file placements to be under test folder rather than main

---
+++ planetarium/src/test/java/com/revature/steps/PlanetandMoonVisibilitySteps.java
@@ -0,0 +1,10 @@
+package com.revature.steps;
+
+import io.cucumber.java.en.Given;
+
+public class PlanetandMoonVisibilitySteps {
+    @Given("The user is on the login page {string}")
+    public void theUserIsOnTheLoginPage(String arg0) {
+
+    }
+}
---
+++ planetarium/src/test/java/com/revature/steps/UserLoginSteps.java
@@ -0,0 +1,66 @@
+package com.revature.steps;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class UserLoginSteps {
+    @When("The user's password is not visible on the text input field")
+    public void the_user_s_password_is_not_visible_on_the_text_input_field() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("The user should be {string} and user's password is not visible")
+    public void the_user_should_be_and_user_s_password_is_not_visible(String string, String docString) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("The user should be {string} to their home page")
+    public void the_user_should_be_to_their_home_page(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("The user should be {string}")
+    public void the_user_should_be(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("The user is {string}")
+    public void the_user_is(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("The user is {string} to their home page")
+    public void the_user_is_to_their_home_page(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("The user opens a new window")
+    public void the_user_opens_a_new_window() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("The user goes to the home page {string}")
+    public void the_user_goes_to_the_home_page(String string, String docString) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("The user should be on the home page")
+    public void the_user_should_be_on_the_home_page(String docString) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("The user should be not on the home page")
+    public void the_user_should_be_not_on_the_home_page(String docString) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
