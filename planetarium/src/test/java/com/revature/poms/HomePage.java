62a589df68f4b4f66c020c1b4d7c015e7a4a0146
Henry Ni <143765606+IAmAsleep@users.noreply.github.com>
2024-11-11 13:06:27 -0500

    Update HomePage.java

--- planetarium/src/test/java/com/revature/poms/HomePage.java
+++ planetarium/src/test/java/com/revature/poms/HomePage.java
@@ -2,6 +2,7 @@ package com.revature.poms;
 
 import java.util.List;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -54,4 +55,88 @@ public class HomePage {
         PageFactory.initElements(driver, this);
     }
 
+    public String returnTitle(){
+        return greetings.getText();
+    }
+
+    public void clickLogout(){
+        this.logoutButton.click();
+    }
+
+    public void changeToMoon(){
+        moonOrPlanet.selectByVisibleText("Moon");
+    }
+
+    public void changeToPlanet(){
+        moonOrPlanet.selectByVisibleText("Planet");
+    }
+
+    public void deleteCelestial(String celestialName){
+        deleteInput.sendKeys(celestialName);
+    }
+
+    public void pressDelete(){
+        this.deleteButton.click();
+    }
+
+    public void addingPlanetName(String celestialName){
+        planetNameInput.sendKeys(celestialName);
+    }
+
+    public void addingPlanetImage(String filepath){
+        planetImageInput.sendKeys(filepath);
+    }
+
+    public void addingMoonName(String celestialName){
+        moonNameInput.sendKeys(celestialName);
+    }
+
+    public void addingMoonImage(String filepath){
+        moonNameInput.sendKeys(filepath);
+    }
+
+    public void addingPlanetID(String id){
+        orbitedPlanetInput.sendKeys(id);
+    }
+
+    public void pressSubmitButton(){
+        createButton.click();
+    }
+
+    public List<WebElement> getTable(){
+        return rows;
+    }
+
+    public boolean confirmPlanet(String celestialName){
+        List <WebElement> table = driver.findElements(By.xpath("//*[@id='celestialTable']/tbody/tr[position()>1]" ));
+        for (WebElement row : table){
+            if(row.findElement(By.xpath("//td[3]")).getText().equals(celestialName)&&
+                    row.findElement(By.xpath("//td[1]")).getText().equals("planet")){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean confirmMoon(String celestialName){
+        List <WebElement> table = driver.findElements(By.xpath("//*[@id='celestialTable']/tbody/tr[position()>1]" ));
+        for (WebElement row : table){
+            if(row.findElement(By.xpath("//td[3]")).getText().equals(celestialName)&&
+                    row.findElement(By.xpath("//td[1]")).getText().equals("moon")){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean confirmOrbitPlanet(String celestialName, int planetID){
+        List <WebElement> table = driver.findElements(By.xpath("//*[@id='celestialTable']/tbody/tr[position()>1]" ));
+        for (WebElement row : table){
+            if(row.findElement(By.xpath("//td[3]")).getText().equals(celestialName)&&
+                    row.findElement(By.xpath("//td[4]")).getText().equals(String.valueOf(planetID))){
+                return true;
+            }
+        }
+        return false;
+    }
 }
