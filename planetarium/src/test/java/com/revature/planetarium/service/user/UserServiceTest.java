79c7859492352e7a39c86e6d4f8976f68def6a8c
Thomas Kay <thomas605@revature.net>
2024-11-20 14:37:43 -0500

    Implemented Junit test for User Controller

--- planetarium/src/test/java/com/revature/planetarium/service/user/UserServiceTest.java
+++ planetarium/src/test/java/com/revature/planetarium/service/user/UserServiceTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.mockito.Mockito;
 
 import com.revature.planetarium.entities.User;
+import com.revature.planetarium.exceptions.UserFail;
 import com.revature.planetarium.repository.user.UserDao;
 
 import java.util.Optional;
@@ -15,126 +16,100 @@ public class UserServiceTest {
 
     private static final String GOOD_USERNAME = "The Long Quiet";
     private static final String GOOD_PASSWORD = "Slay the princess";
+    private static final String EXISTING_USERNAME = "Batman";
 
     private static UserService userService;
     private static UserDao userDao;
+    private static User testUser;
 
     @Before
     public void setupForEachTest() {
         userDao = Mockito.mock(UserDao.class);
         userService = new UserServiceImp(userDao);
+        testUser = new User();
+        testUser.setUsername(GOOD_USERNAME);
+        testUser.setPassword(GOOD_PASSWORD);
     }
 
     @Test
     public void positiveCreateUserTest() {
-        String username = GOOD_USERNAME;
-        String password = GOOD_PASSWORD;
-        User inputUser = new User();
-        inputUser.setUsername(username);
-        inputUser.setPassword(password);
-        Mockito.when(userDao.createUser(inputUser)).thenReturn(Optional.of(inputUser));
-        String expectedMessage = "Account created successfully with username \"" + username + "\"";
-        String actualMessage = userService.createUser(inputUser);
-        // include or exclude?
-        Mockito.verify(userDao, Mockito.times(1)).createUser(inputUser);
+        Mockito.when(userDao.createUser(testUser)).thenReturn(Optional.of(testUser));
+        Mockito.when(userDao.findUserByUsername(testUser.getUsername())).thenReturn(
+            Optional.empty());
+        String expectedMessage = "Created user with username " + GOOD_USERNAME + " and password " +
+            GOOD_PASSWORD;
+        String actualMessage = userService.createUser(testUser);
         Assert.assertEquals(expectedMessage, actualMessage);
     }
 
     @Test
     public void negativeCreateUserTestUsernameLength0() {
-        String username = "";
-        String password = GOOD_PASSWORD;
-        User inputUser = new User();
-        inputUser.setUsername(username);
-        inputUser.setPassword(password);
-        Mockito.when(userDao.createUser(inputUser)).thenReturn(Optional.empty());
-        String expectedMessage = "Account creation failed with username \"" + username + "\"";
-        String actualMessage = userService.createUser(inputUser);
-        // include or exclude?
-        Mockito.verify(userDao, Mockito.times(0)).createUser(inputUser);
-        Assert.assertEquals(expectedMessage, actualMessage);
+        testUser.setUsername("");
+        Assert.assertThrows(UserFail.class, () -> {
+            userService.createUser(testUser);
+        });
     }
 
     @Test
     public void negativeCreateUserTestUsernameLength31() {
-        String username = "There is a cabin in the woods..";
-        String password = GOOD_PASSWORD;
-        User inputUser = new User();
-        inputUser.setUsername(username);
-        inputUser.setPassword(password);
-        Mockito.when(userDao.createUser(inputUser)).thenReturn(Optional.empty());
-        String expectedMessage = "Account creation failed with username \"" + username + "\"";
-        String actualMessage = userService.createUser(inputUser);
-        // include or exclude?
-        Mockito.verify(userDao, Mockito.times(0)).createUser(inputUser);
-        Assert.assertEquals(expectedMessage, actualMessage);
+        testUser.setUsername("The pristine blade is gleaming!");
+        Assert.assertThrows(UserFail.class, () -> {
+            userService.createUser(testUser);
+        });
     }
 
     @Test
     public void negativeCreateUserTestPasswordLength0() {
-        String username = GOOD_USERNAME;
-        String password = "";
-        User inputUser = new User();
-        inputUser.setUsername(username);
-        inputUser.setPassword(password);
-        Mockito.when(userDao.createUser(inputUser)).thenReturn(Optional.empty());
-        String expectedMessage = "Account creation failed with username \"" + username + "\"";
-        String actualMessage = userService.createUser(inputUser);
-        // include or exclude?
-        Mockito.verify(userDao, Mockito.times(0)).createUser(inputUser);
-        Assert.assertEquals(expectedMessage, actualMessage);
+        testUser.setPassword("");
+        Assert.assertThrows(UserFail.class, () -> {
+            userService.createUser(testUser);
+        });
     }
 
     @Test
     public void negativeCreateUserTestPasswordLength31() {
-        String username = GOOD_USERNAME;
-        String password = "Slay, not romance the princess!";
-        User inputUser = new User();
-        inputUser.setUsername(username);
-        inputUser.setPassword(password);
-        Mockito.when(userDao.createUser(inputUser)).thenReturn(Optional.empty());
-        String expectedMessage = "Account creation failed with username \"" + username + "\"";
-        String actualMessage = userService.createUser(inputUser);
-        // include or exclude?
-        Mockito.verify(userDao, Mockito.times(0)).createUser(inputUser);
-        Assert.assertEquals(expectedMessage, actualMessage);
+        testUser.setPassword("Slay, not romance the princess!");
+        Assert.assertThrows(UserFail.class, () -> {
+            userService.createUser(testUser);
+        });
     }
 
     @Test
     public void negativeCreateUserTestDuplicateUsername() {
-        String username = "Batman";
-        String password = GOOD_PASSWORD;
-        User inputUser = new User();
-        inputUser.setUsername(username);
-        inputUser.setPassword(password);
-        Mockito.when(userDao.createUser(inputUser)).thenReturn(Optional.empty());
-        String expectedMessage = "Account creation failed with username \"" + username + "\"";
-        String actualMessage = userService.createUser(inputUser);
-        // include or exclude?
-        Mockito.verify(userDao, Mockito.times(0)).createUser(inputUser);
-        Assert.assertEquals(expectedMessage, actualMessage);
+        testUser.setUsername(EXISTING_USERNAME);
+        Mockito.when(userDao.findUserByUsername(testUser.getUsername())).thenReturn(
+            Optional.of(testUser));
+        Assert.assertThrows(UserFail.class, () -> {
+            userService.createUser(testUser);
+        });
     }
 
     @Test
     public void positiveAuthenticateTest() {
-        String username = "Batman";
-        String password = "I am the night";
-        User expectedUser = new User();
-        expectedUser.setUsername(username);
-        expectedUser.setPassword(password);
-        Mockito.when(userDao.findUserByUsername(username)).thenReturn(Optional.of(expectedUser));
-        User actualUser = userService.authenticate(expectedUser);
-        Assert.assertEquals(expectedUser, actualUser);
+        Mockito.when(userDao.findUserByUsername(testUser.getUsername())).thenReturn(
+            Optional.of(testUser));
+        User actualUser = userService.authenticate(testUser);
+        Assert.assertEquals(testUser, actualUser);
     }
 
     @Test
-    public void negativeAuthenticateTestIncorrectUsername() {
-        String username = "Robin";
-        String password = "I am the night";
-        User inputUser = new User();
-        inputUser.setUsername(username);
-        inputUser.setPassword(password);
-        User returnedUser = new User();
-        Mockito.when(userDao.findUserByUsername(username)).thenReturn(Optional.of(inp))
+    public void negativeAuthenticateTestWrongUsername() {
+        Mockito.when(userDao.findUserByUsername(testUser.getUsername())).thenReturn(
+            Optional.empty());
+        Assert.assertThrows(UserFail.class, () -> {
+            userService.authenticate(testUser);
+        }); 
     }
+
+    @Test
+    public void negativeAuthenticateTestWrongPassword() {
+        User returnedUser = new User();
+        returnedUser.setPassword("The cycle never ends");
+        Mockito.when(userDao.findUserByUsername(testUser.getUsername())).thenReturn(
+            Optional.of(returnedUser));
+        Assert.assertThrows(UserFail.class, () -> {
+            userService.authenticate(testUser);
+        });
+    }
+
 }
